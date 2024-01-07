3d8b1b5f76003f454fa09eb699a4eb63cbb8737b
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2024-01-07 22:31:53 +0000

    pycode style solved

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,6 +19,7 @@ def close_storage(exception):
 
     storage.close()
 
+
 if __name__ == "__main__":
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
     port = os.environ.get('HBNB_API_PORT', '5000')
